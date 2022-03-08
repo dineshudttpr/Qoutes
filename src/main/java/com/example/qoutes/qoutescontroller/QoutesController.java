f890dce58326cc705e85ea405bc7c62c9fe93557
dineshudttpr <68327688+dineshudttpr@users.noreply.github.com>
2022-03-08 12:06:51 +0530

    Update QoutesController.java

--- src/main/java/com/example/qoutes/qoutescontroller/QoutesController.java
+++ src/main/java/com/example/qoutes/qoutescontroller/QoutesController.java
@@ -6,8 +6,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class QoutesController {
 
-    @GetMapping("/qoutes")
-    public String getQoutes(){
-        return "Created";
+   @GetMapping("/qoutes/{id}")
+    public String getQoutes(@PathVariable int id) throws IOException {
+
+        InputStream inputStream = getClass()
+                .getClassLoader().getResourceAsStream("data.txt");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+
+        String line ;
+        int count =1;
+        while ((line = br.readLine()) != null) {
+           if(count == id){
+             return line;
+           }
+           count++;
+        }
+
+        return "Not found";
     }
 }
